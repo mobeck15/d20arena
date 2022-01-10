f17bceca641c66fec5d6a7f06f33832c8e4a1244
Isaac Guerrero <mobeck@gmail.com>
2022-01-09 20:42:26 -0800

    Setting up data

---
+++ index.php
@@ -0,0 +1,28 @@
+<?php
+$GLOBALS['rootpath']=$GLOBALS['rootpath'] ?? ".";
+require_once $GLOBALS['rootpath']."/inc/main.inc.php";
+
+?>
+<ul><li>Spreadsheets
+<ul>
+<li><a href="https://docs.google.com/spreadsheets/d/1sH8enARVVcr3MG4RIXrLIzhifzRl5afGKgG0GIfbnok/edit#gid=1111386538">Monster Table</a></li>
+<li><a href="https://docs.google.com/spreadsheets/d/1cT1bRihnc5dGSoEg3Qtg0DQ3l-i1j6E9HnixnTzFQd8/edit#gid=0">Monster List (All)</a></li>
+<li><a href="https://docs.google.com/spreadsheets/d/1Jp9LRXF_qofvxreJVWpBmlmwEMQ8vnQ7uuwAnd6ZKWE/edit#gid=1834798233">D&D 3E Monsters (New Sheets)</a></li>
+<li><a href="https://docs.google.com/spreadsheets/d/1ESSc-wiNu77Enb7izXCVGyCpzy4fRKEsgEfs2vX6AIA/edit#gid=3">D&D 3E Monsters</a></li>
+<li><a href="https://docs.google.com/spreadsheets/d/1S9ouUHRzgCnnOziGA0VxGPRaGwu-uiebKOlcdDOn1IY/edit#gid=0">3.5e Data</a></li>
+<li><a href="https://docs.google.com/spreadsheets/d/1I-W7JNW0yqqAJGDcR5XUe2Kzsqo4d9cJbBiGuk3tR_E/edit#gid=0">Dungeon Creation</a></li>
+<li><a href="https://docs.google.com/spreadsheets/d/1w1THKz22Wh-Fzi-YIjzETqwyHmCbWtvzOFThOq2apsA/edit#gid=0">Little Piece of Hell v2</a></li>
+<li><a href="https://docs.google.com/spreadsheets/d/142Xq8EGISsJYyQ_4kAHW70Mp9oLTGjPnexUzfncQGtk/edit#gid=0">Treasure Generator</a></li>
+<li><a href="https://docs.google.com/spreadsheets/d/1P8lDZICIj9Jd6nxv1UPno_vwkhNro2YTJUERzb87tzA/edit#gid=217121210">Little Piece of Hell v3</a></li>
+<li><a href="https://docs.google.com/spreadsheets/d/1M5RCuo7bYi-JZ-tEtaO-m590vTIOQdFUOaoseQ1WWUE/edit#gid=1636444334">Magic_items.xls</a></li>
+<li><a href="https://docs.google.com/spreadsheets/d/1NKuIlR5P0xTk8Rthete5lVDvzQhy9i1agXWMgRzU_n4/edit#gid=1935830453">Complete D&D v3.5 Monster index</a></li>
+<li><a href="https://docs.google.com/spreadsheets/d/1kcWmYlP7Bt6i--l9762bHJ81T8pDA80PLeAPEFPHCWg/edit#gid=0">Crystalwell City</a></li>
+</ul></li>
+<li>Websites
+<ul>
+<li><a href="https://www.d20srd.org/index.htm">D20 SRD</a></li>
+</ul></li>
+<li>Websites
+<ul>
+<li><a href=""></a></li>
+</ul></li>
